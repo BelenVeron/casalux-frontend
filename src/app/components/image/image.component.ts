96e19abfc03138dda5c032867bc9343c7e487f8c
dante.panella.contractor.gdevtools.com <dante.panella.contractor@gdevtools.com>
2022-03-27 17:03:27 -0300

    cambios en loading

--- src/app/components/image/image.component.ts
+++ src/app/components/image/image.component.ts
@@ -7,9 +7,6 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ImageComponent implements OnInit {
 
-  @Input() loader:string='https://media.tenor.com/images/f864cbf3ea7916572605edd3b3fe637f/tenor.gif';
-  @Input() height:number=200;
-  @Input() width:number=200;
   @Input() src: string = '';
   @Input() type: string = '';
   @Input() text: string = '';
